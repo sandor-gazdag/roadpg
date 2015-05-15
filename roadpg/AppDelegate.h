c1c16bcac7b1136bb782d16896179d1c79ece97a
Sandor Gazdag <sandor_gazdag@epam.com>
2015-05-15 17:20:39 +0200

    Initial Commit

---
+++ roadpg/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  roadpg
+//
+//  Created by sgazdag on 15/05/15.
+//  Copyright (c) 2015 sgazdag. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ roadpg/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  roadpg
+//
+//  Created by sgazdag on 15/05/15.
+//  Copyright (c) 2015 sgazdag. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
